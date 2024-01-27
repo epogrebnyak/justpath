cc82a1b0f36a84fc2951674f6c5e15e9c6fc66aa
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2024-01-27 13:15:26 +0300

    help messages for --help flag

--- pathit/show.py
+++ pathit/show.py
@@ -1,7 +1,11 @@
+"""Show PATH variable by line."""
+
 import os
 import typer
 
-app = typer.Typer()
+app = typer.Typer(add_completion=False, 
+                  help="Just show me my PATH variable.",
+                  short_help="Just show the PATH.")
 
 
 def get_var(name, sep):
@@ -17,5 +21,10 @@ def print_sorted(sort: bool = False, includes: str | None = None):
     print("\n".join(paths))
 
 
+# https://github.com/tiangolo/typer/blob/3a7264cd56181690805f220cb44a0301c0fdf9f3/typer/main.py#L1053
+# def run(function: Callable[..., Any]) -> None:
+#     app = Typer(add_completion=False)
+#     app.command()(function)
+#     app()
 def typer_app():
     typer.run(print_sorted)
